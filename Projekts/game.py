d2da4b528c365cb7b0d5e2a2c4aa0591fee5019d
Markuss Spricis <markuss5764@gmail.com>
2024-06-09 20:44:44 +0300

    Kustība un ierobežojums līdz grīdas malām

--- Projekts/game.py
+++ Projekts/game.py
@@ -20,3 +20,100 @@ for pos in light_positions:
     point_lights.append(point_light)
 
 #---------------------------------------------------------------------------
+
+# Kustība un limits līdz grīdas malām------------------------------------------------------------------------------------------
+
+# Grīdas izmērs
+floor_size = 30
+
+# Tinker objekts
+tinkercad_model = vizfx.addChild(r'C:\Users\marku\Downloads\Tremendous Crift\tinker.obj')
+tinkercad_model.setScale(0.025, 0.025, 0.025)
+tinkercad_model.setEuler(90, 90, 0)
+tinkercad_model.color(viz.GREEN)
+
+# Kustības parametri
+MOVE_SPEED = 2.0
+CAMERA_DISTANCE = 5.0
+CAMERA_HEIGHT = 2.0
+
+# Taustiņu stāvokļa izsekošana
+keys = {
+    'up': False,
+    'down': False,
+    'left': False,
+    'right': False
+}
+
+# Apakšējā taustiņa darbība
+def onKeyDown(key):
+    if key == viz.KEY_UP:
+        keys['up'] = True
+    elif key == viz.KEY_DOWN:
+        keys['down'] = True
+    elif key == viz.KEY_LEFT:
+        keys['left'] = True
+    elif key == viz.KEY_RIGHT:
+        keys['right'] = True
+
+# Augšējā taustiņa darbība
+def onKeyUp(key):
+    if key == viz.KEY_UP:
+        keys['up'] = False
+    elif key == viz.KEY_DOWN:
+        keys['down'] = False
+    elif key == viz.KEY_LEFT:
+        keys['left'] = False
+    elif key == viz.KEY_RIGHT:
+        keys['right'] = False
+
+# Objekta pozīcijas atjaunināšana
+def updateObject():
+    global tinkercad_model
+    euler = tinkercad_model.getEuler()
+    pos = tinkercad_model.getPosition()
+
+    # Kustības virziena aprēķināšana
+    move_direction = [0, 0]
+    if keys['up']:
+        move_direction[1] = 1
+    elif keys['down']:
+        move_direction[1] = -1
+    if keys['left']:
+        move_direction[0] = -1
+    elif keys['right']:
+        move_direction[0] = 1
+
+    # Jaunās pozīcijas aprēķināšana
+    new_x = pos[0] + move_direction[0] * MOVE_SPEED * viz.elapsed()
+    new_z = pos[2] + move_direction[1] * MOVE_SPEED * viz.elapsed()
+
+    # Ierobežo kustību līdz grīdas malām
+    half_size_x = floor_size / 2 - 0.5
+    half_size_z = floor_size / 2 - 0.5
+    new_x = max(-half_size_x, min(half_size_x, new_x))
+    new_z = max(-half_size_z, min(half_size_z, new_z))
+
+    # Iestata jauno pozīciju
+    tinkercad_model.setPosition([new_x, pos[1], new_z])
+
+    # Atjauno kameras pozīciju
+    updateCamera([new_x, pos[1], new_z], euler[0])
+
+# Kamera seko objektam
+def updateCamera(pos, angle):
+    # Kameras pozīcijas aprēķināšana aiz objekta
+    camera_pos = [
+        pos[0] - CAMERA_DISTANCE * math.sin(math.radians(angle)),
+        pos[1] + CAMERA_HEIGHT,
+        pos[2] - CAMERA_DISTANCE * math.cos(math.radians(angle))
+    ]
+    viz.MainView.setPosition(camera_pos)
+    viz.MainView.lookAt(pos)
+
+viz.callback(viz.KEYDOWN_EVENT, onKeyDown)
+viz.callback(viz.KEYUP_EVENT, onKeyUp)
+
+vizact.ontimer(0, updateObject)
+
+#-------------------------------------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -4,22 +4,21 @@ Par spēli:
 
 Piekļūšana darbam:
 
--- Repozitorija noklonēšana - https://github.com/Markussspricis/3D---p-ru-darbs
+1. Repozitorija noklonēšana - https://github.com/Markussspricis/3D---p-ru-darbs
 
-Vajadzīgo moduļu un valodu instalēšana:
+-- Nokopēt kodu no game.py faila un pārnest uz Vizard 7 vidi
 
-1. Python:
+2. Koda kopēšana GitHub vidē failā game.py projekta repozitorijā
 
--- Lejupielādēt Python no https://www.python.org/downloads/
+-- Nokopēt kodu no game.py faila un pārnest uz Vizard 7 vidi
 
-2. viz:
+Vajadzīgo rīku lejupielāde:
 
--- Lejupielādēt viz pakotnes pirmkodu no https://pypi.org/project/viz/#files
--- Izvilkt .tar.gz failu uz projekta repozitoriju, izmantojot komandu - tar -xzf viz-0.0.5.tar.gz
--- Atvērt setup.py
--- Nomainīt šo rindu: long_description=open('README.rst').read(), pret šo: long_description=open('README.rst', encoding='utf-8').read(),
--- Palaist komandu: pip install .
+1. Vizard 7:
+
+-- Lejupielādēt Vizard 7 no https://www.worldviz.com/virtual-reality-software-downloads
+-- Instalēt Vizard 7 uz sava datora
 
 Spēles palaišana:
 
---
+-- Izveidot jaunu vizard failu, iekopēt iegūto kodu no GitHub un palaist failu, spiežot uz rīku joslā esošās zaļās bultas
